8d149b89cd1db28c42b2a7d9d5560843dff22361
BestSTN <bestflashcs3@hotmail.com>
2022-10-02 19:36:41 +0700

    feature register and login

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
+const authRoute = require("./routes/authRoute");
 const error = require("./middlewares/error");
 const notFound = require("./middlewares/notFound");
 
@@ -16,12 +17,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use("/auth", authRoute);
+
 app.use(error);
 app.use(notFound);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`server is running on port: ${port}`));
 
-
-const { sequelize } = require("./models");
-sequelize.sync({ alter: true });
+// const { sequelize } = require("./models");
+// sequelize.sync({ force: true });
---
+++ src/controllers/authControllers.js
@@ -0,0 +1,82 @@
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { Op } = require("sequelize");
+
+const { User } = require("../models");
+const AppError = require("../utils/appError");
+
+const genToken = (payload) =>
+  jwt.sign(payload, process.env.JWT_SECRET_KEY || "secret_key", {
+    expiresIn: process.env.JWT_EXPIRES || "1d",
+  });
+
+exports.register = async (req, res, next) => {
+  try {
+    const { firstName, lastName, username, email, password, confirmPassword } =
+      req.body;
+
+    if (!firstName || firstName.trim()) {
+      throw new AppError("firstName is required", 400);
+    }
+    if (!lastName || lastName.trim()) {
+      throw new AppError("lastName is required", 400);
+    }
+    if (!username || username.trim()) {
+      throw new AppError("username is required", 400);
+    }
+    if (password !== confirmPassword) {
+      throw new AppError("password and confirm password did not match", 400);
+    }
+    if (!validator.isEmail(email)) {
+      throw new AppError("email is invalid", 400);
+    }
+    const isThereUser = await User.findOne({
+      where: {
+        [Op.or]: [{ username }, { email }],
+      },
+    });
+    if (isThereUser) {
+      throw new AppError("already have this username or email", 400);
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      firstName,
+      lastName,
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = genToken({ id: user.id });
+
+    res.status(201).json({ token });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { usernameOrEmail, password } = req.body;
+
+    const user = await User.findOne({
+      where: {
+        [Op.or]: [{ username: usernameOrEmail }, { email: usernameOrEmail }],
+      },
+    });
+    if (!user) {
+      throw new AppError("email address or mobile or password is invalid", 400);
+    }
+    const isCorrectPassword = await bcrypt.compare(password, user.password);
+    if (!isCorrectPassword) {
+      throw new AppError("email address or mobile or password is invalid", 400);
+    }
+
+    const token = genToken({ id: user.id });
+    res.status(200).json({ token });
+  } catch (err) {
+    next(err);
+  }
+};
